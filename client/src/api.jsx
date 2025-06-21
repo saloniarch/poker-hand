8da0791ef259f5d4e6599a343e3f84d9220fc20f
Your Name <saloni.arch@gmail.com>
2025-06-21 21:51:40 +0200

    Docker Container worked!

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-    const API_URL = "http://localhost:3001";
+    const API_URL = import.meta.env.VITE_API_URL;
 
     export const api = () => {
         const get = async (endpoint) => {
